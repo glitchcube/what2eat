23c0f2ad2b20a1f9142748ad4cc16a8e5dc0e36a
Frank Navarsete <frank.navarsete@gmail.com>
2024-09-27 00:13:08 +0200

    add PWA

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "what2eat-cache-v1";
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/assets/images/logo-192x192.png",
+    "/assets/images/logo-512x512.png",
+    "/assets/images/logo.svg",
+    "/script.js",
+    "/manifest.json",
+];
+
+// Install service worker and cache files
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Serve files from cache
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Update service worker
+self.addEventListener("activate", (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
